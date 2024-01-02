54ba94966d621bed476ee8b4d5f2241918bb3c23
unknown <myanasrija@gmail.com>
2024-01-02 02:59:55 -0500

    update app.js

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ const campgroundRoutes = require('./routes/campgrounds.js');
 const reviewRoutes = require('./routes/reviews.js');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
-// const dbUrl = process.env.DB_URL
-const dbUrl = 'mongodb://localhost:27017/yelp-camp';
+
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
 mongoose.connect(dbUrl);
 
@@ -43,11 +43,13 @@ app.use(methodOverride('_method'));
 
 app.use(express.static(path.join(__dirname, 'public')))
 
+const secret = process.env.SECRET || 'thishouldbeabettersecret';
+
 const store = MongoDBStore.create({
     mongoUrl: dbUrl,
     touchAfter: 24 * 60 * 60,
     crypto: {
-        secret: 'thisshouldbeabettersecret'
+        secret: secret
     }
 });
 
@@ -58,7 +60,7 @@ store.on("error", function(e) {
 const sessionConfig = {
     store,
     name: 'session',
-    secret: 'thishouldbeabettersecret',
+    secret: secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
